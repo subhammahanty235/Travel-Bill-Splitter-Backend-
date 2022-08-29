8acd3f9c4b01b3e01d66db419b48d8aa5a09f83f
subhammahanty235 <subhammahanty235@gmail.com>
2022-08-30 01:25:15 +0530

    trying to solve cors

--- index.js
+++ index.js
@@ -6,9 +6,21 @@ const cors = require('cors');
 const connectToDB = require('./db');
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 connectToDB();
+const whitelist = ["http://localhost:3000"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
+
 
 app.use('/api/v1' , require('./routes/userRoutes'))
 app.use('/api/v1' , require('./routes/tripRoutes'))
