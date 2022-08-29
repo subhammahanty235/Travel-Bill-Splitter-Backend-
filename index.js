495608882cc463ae3bc968280f4a767efdfb4ec6
subhammahanty235 <subhammahanty235@gmail.com>
2022-08-30 01:35:50 +0530

    b

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ const app = express();
 
 app.use(express.json());
 connectToDB();
-// const whitelist = ["http://localhost:3000"]
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (!origin || whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error("Not allowed by CORS"))
-//     }
-//   },
-//   credentials: true,
-// }
-// app.use(cors(corsOptions))
+const whitelist = ["http://localhost:3000"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
 
 
 app.use('/api/v1' , require('./routes/userRoutes'))
