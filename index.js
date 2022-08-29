a60e1c44f7c3b6c15d296a867afc46711fecef40
subhammahanty235 <subhammahanty235@gmail.com>
2022-08-30 01:44:02 +0530

    ab

--- index.js
+++ index.js
@@ -20,7 +20,13 @@ const corsOptions = {
   credentials: true,
 }
 app.use(cors(corsOptions))
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 app.use('/api/v1' , require('./routes/userRoutes'))
 app.use('/api/v1' , require('./routes/tripRoutes'))
